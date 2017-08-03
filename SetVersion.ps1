c64901435e0560d9612c8b78dc2baf2d83f48103
Spencer Bloom <spebl@microsoft.com>
2017-08-03 14:48:05 -0700

    Set binary and VSIX versions

---
+++ SetVersion.ps1
@@ -0,0 +1,16 @@
+Param([parameter(Mandatory=$true)] [string] $version)
+
+$files_to_update = @(
+  "Antlr.DOT\Properties\AssemblyInfo.cs",
+  "BoostTestAdapter\Properties\AssemblyInfo.cs",
+  "BoostTestPackage\Properties\AssemblyInfo.cs",
+  "BoostTestPlugin\Properties\AssemblyInfo.cs",
+  "BoostTestPlugin\source.extension.vsixmanifest",
+  "BoostTestShared\Properties\AssemblyInfo.cs",
+  "VisualStudio2015Adapter\Properties\AssemblyInfo.cs",
+  "VisualStudioAdapter\Properties\AssemblyInfo.cs"
+)
+
+$files_to_update | ForEach-Object {
+  (Get-Content $_) | ForEach-Object { $_.replace("0.0.0.0", $version) } | Set-Content $_
+}
