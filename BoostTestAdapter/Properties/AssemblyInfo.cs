0a8fce1ec67536f8e1aaba6d94dd766701722c97
Spencer Bloom <spebl@microsoft.com>
2017-08-04 09:06:31 -0700

    Sign binaries

--- BoostTestAdapter/Properties/AssemblyInfo.cs
+++ BoostTestAdapter/Properties/AssemblyInfo.cs
@@ -41,5 +41,8 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.0.0.0")]
 [assembly: AssemblyFileVersion("0.0.0.0")]
-[assembly: InternalsVisibleTo("BoostTestAdapterNunit")]
 [assembly: DefaultDllImportSearchPaths(DllImportSearchPath.SafeDirectories)]
+
+#if !REALSIGN
+[assembly: InternalsVisibleTo("BoostTestAdapterNunit")]
+#endif
