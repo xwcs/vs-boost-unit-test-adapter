a9766697e4d19d42806e7f06fb2052f80a34fa18
Spencer Bloom <spebl@microsoft.com>
2017-11-29 16:28:41 -0800

    move null check out of loop

--- BoostTestAdapter/Utility/DebugHelperNative.cs
+++ BoostTestAdapter/Utility/DebugHelperNative.cs
@@ -384,7 +384,8 @@ namespace BoostTestAdapter.Utility
                 bool shouldContinue = true;
                 uint size = 0u;
                 var directoryEntry = (NativeMethods.IMAGE_IMPORT_DESCRIPTOR*)NativeMethods.ImageDirectoryEntryToData(image.MappedAddress, 0, 1, &size);
-                while (shouldContinue && directoryEntry != null && directoryEntry->OriginalFirstThunk != 0u)
+                if (directoryEntry == null) return;
+                while (shouldContinue && directoryEntry->OriginalFirstThunk != 0u)
                 {
                     shouldContinue = predicate(GetString(image, directoryEntry->Name));
                     directoryEntry++;
